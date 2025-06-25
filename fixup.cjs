f3e8a82837b95e568795302ed0795b35dc6d2661
klouskingsley <12490787+klouskingsley@users.noreply.github.com>
2025-06-25 16:40:48 +0800

    update version

--- fixup.cjs
+++ fixup.cjs
@@ -3,12 +3,13 @@ const path = require('path');
 
 const updateRequires = (filePath) => {
   let content = fs.readFileSync(filePath, 'utf8');
-  console.log(filePath);
-  //replace local imports eg. require("./ecpair.js") to require("ecpair.cjs")
-  content = content.replace(/require\("\.\/([^"]*)\.js"\)/g, "require('./$1.cjs')");
-  content = content.replace(/require\("\.\.\/([^"]*)\.js"\)/g, "require('../$1.cjs')");
-
-  fs.writeFileSync(filePath, content, 'utf8');
+  //replace local imports eg. require("./yyy/ecpair.js") to require("yyy/ecpair.cjs")
+  //replace local imports eg. require("../xx/ecpair.js") to require("../xx/ecpair.cjs")
+  const updatedCode = content.replace(/require\(\s*["']([^"']+\.js)["']\s*\)/g, (match, p1) => {
+    // replace .js to .cjs
+    return `require('${p1.replace(/\.js$/, '.cjs')}')`;
+  });
+  fs.writeFileSync(filePath, updatedCode, 'utf8');
 };
 
 const processFiles = (dir) => {
@@ -16,8 +17,11 @@ const processFiles = (dir) => {
     const filePath = path.join(dir, file);
     if (fs.lstatSync(filePath).isDirectory()) {
       processFiles(filePath);
-    } else if (filePath.endsWith('.cjs')) {
+    } else if (filePath.endsWith('.js')) {
       updateRequires(filePath);
+      const fileName = path.basename(filePath);
+      const dir = path.dirname(filePath);
+      fs.renameSync(filePath, path.join(dir, fileName.replace('.js', '.cjs')));
     }
   });
 };
